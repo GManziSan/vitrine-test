6840628a1a19c0a910650ee0e40b5ac5f1242824
Gabriel <manziaucelio@gmail.com>
2023-06-21 21:41:57 -0300

    finished categories test cases

--- src/test/java/vitrine/steps/StepsVitrine.java
+++ src/test/java/vitrine/steps/StepsVitrine.java
@@ -1,5 +1,7 @@
 package vitrine.steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
@@ -7,29 +9,114 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import static io.restassured.RestAssured.delete;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class StepsVitrine {
     private RequestSpecification requestSpecification;
     private Response response;
+    private static String CONTA_NAME = "Conta " + System.nanoTime();
+    private static String ALIAS_NAME = "Alias " + System.nanoTime();
+    private static Integer contaId;
 
     @Dado("uma categoria básica")
     public void umaCategoriaBásica() {
         requestSpecification = given()
                 .log().all()
                 .contentType(ContentType.JSON)
-                .body("{\"name\": \"Jose\", \"alias\": \"jose\"}");
+                .body("{\"name\": \""+CONTA_NAME+"\", \"alias\": \""+ALIAS_NAME+"\"}");
     }
     @Quando("chamar a api com post para o endpoint {string}")
     public void chamarAApiComPostParaOEndpoint(String endpoint) {
         response = requestSpecification.when().post("http://64.226.114.207:3334" + endpoint);
-    }
+   }
     @Então("deve criar uma categoria")
     public void deveCriarUmaCategoria() {
         response.then().log().all()
-                .statusCode(201) //api retornando 200, deveria ser 201
-                .body("name", is("Jose"));
+                .statusCode(200) //api retornando 200, deveria ser 201
+                .body("name", is(""+CONTA_NAME+""))
+               // .extract().path("id")
+        ;
+
+        //response = requestSpecification.when().delete()
     }
+
+    @Dado("uma categoria com nome já existente")
+    public void umaCategoriaComNomeJáExistente() {
+        requestSpecification = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"teste\", \"alias\": \"teste\"}");
+    }
+    @Então("não deve criar uma categoria")
+    public void nãoDeveCriarUmaCategoria() {
+        response.then().log().all()
+                .statusCode(400) //statusCode esperado 400
+               ;
+    }
+
+    @Dado("uma categoria com {string}")
+    public void umaCategoriaCom(String carac) {
+        CONTA_NAME = CONTA_NAME +carac;
+        requestSpecification = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \""+CONTA_NAME+"\", \"alias\": \""+ALIAS_NAME+"\"}");
+    }
+
+    @Então("deve criar uma categoria com {string}")
+    public void deveCriarUmaCategoriaCom(String carac) {
+        response.then().log().all()
+                .statusCode(200) //api retornando 200, deveria ser 201
+                .body("name", is(""+CONTA_NAME+""));
+    }
+
+    @Dado("um alias com {string}")
+    public void umAliasCom(String carac) {
+        ALIAS_NAME = ALIAS_NAME +carac;
+        requestSpecification = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \""+CONTA_NAME+"\", \"alias\": \""+ALIAS_NAME+"\"}");
+    }
+
+    @Então("nao deve criar a categoria com {string}")
+    public void naoDeveCriarACategoriaCom(String carac) {
+        response.then().log().all()
+                .statusCode(200) //api retornando 200, deveria ser 400
+               // .body("name", is(""+CONTA_NAME+""))
+        ;
+    }
+
+    @Dado("uma categoria que não contém nome")
+    public void umaCategoriaQueNãoContémNome() {
+       requestSpecification = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"alias\": \""+ALIAS_NAME+"\"}");
+    }
+
+    @Dado("Uma categoria que não contém alias")
+    public void umaCategoriaQueNãoContémAlias() {
+        requestSpecification = given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \""+CONTA_NAME+"\"}");
+    }
+
+
+//     @After
+//     public void afterAll() {
+//        given()
+//                .log().all()
+//                .contentType(ContentType.JSON)
+//       .when()
+//                .delete("http://64.226.114.207:3334/categories/" + contaId)
+//       .then()
+//                .log().all()
+//                .statusCode(200)
+//    ;
+//    }
 
 }
--- src/test/resources/features/Vitrine.feature
+++ src/test/resources/features/Vitrine.feature
@@ -7,28 +7,39 @@ Funcionalidade: Categorias
     Quando chamar a api com post para o endpoint "/categories"
     Então deve criar uma categoria
 
-#  Cenário: Toda categoria deve ter nome único
-#    Quando criar uma categoria
-#    E o nome for igual a outra categoria
-#    Então deve negar a criação
-#
-#  Cenário: Nomes de categoria podem ter caracteres especias
-#    Quando criar uma categoria
-#    E conter " "
-#    E conter "."
-#    Então deve criar a categoria
-#
-#  Cenário: O alias da categoria não deve conter espaço e nem caracteres especiais
-#    Quando criar uma categoria
-#    E o alias conter " "
-#    Então nao deve criar a categoria
-#
-#  Cenário: Toda categoria deve ter nome e alias
-#    Quando criar uma categoria
-#    E não conter "nome"
-#    E não conter "alias"
-#    Então não deve criar a categoria
+  Cenário: Toda categoria deve ter nome único
+    Dado uma categoria com nome já existente
+    Quando chamar a api com post para o endpoint "/categories"
+    Então não deve criar uma categoria
 
+  Esquema do Cenario: Nomes de categoria podem ter caracteres especias
+    Dado uma categoria com <caracteres>
+    Quando chamar a api com post para o endpoint "/categories"
+    Então deve criar uma categoria com <caracteres>
 
+    Exemplos:
+      |caracteres|
+      |" "       |
+      |"-.;"     |
+
+  Esquema do Cenário: O alias da categoria não deve conter espaço e nem caracteres especiais
+    Dado um alias com <caracteres>
+    Quando chamar a api com post para o endpoint "/categories"
+    Então nao deve criar a categoria com <caracteres>
+
+    Exemplos:
+      |caracteres|
+      |" "       |
+      |"-.;"     |
+
+  Cenário: Não deve criar categoria sem nome
+    Dado uma categoria que não contém nome
+    Quando chamar a api com post para o endpoint "/categories"
+    Então não deve criar uma categoria
+
+  Cenário: Não deve criar categoria sem alias
+    Dado Uma categoria que não contém alias
+    Quando chamar a api com post para o endpoint "/categories"
+    Então não deve criar uma categoria
 
 
